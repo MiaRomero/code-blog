4a79a6dba74ae9d52f31657b0f680a3f9e5dca1a
Christopher Lee <yensen@gmail.com>
2015-12-04 15:28:23 -0800

    live preview works with handlebars

---
+++ new.js
@@ -0,0 +1,18 @@
+var newEntry = {};
+
+$('#new-form').change(function() {
+  newEntry.articleTitle = $('#article-title').val();
+  newEntry.category = $('#article-category').val();
+  newEntry.author = $('#article-author').val();
+  newEntry.authorUrl = $('#article-author-url').val();
+  newEntry.publishedOn = new Date();
+  newEntry.articleBody = $('#article-body').val();
+  // console.log(newEntry);
+
+  var previewTemplateScript = $('#article-template').html();
+  var previewTemplate = Handlebars.compile(previewTemplateScript);
+  var previewHTML = previewTemplate(newEntry);
+
+  $('#articles').empty().append(previewHTML);
+
+});
