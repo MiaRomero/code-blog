b111ad8e9aaaf73b1490330ddd0f184e39367d3e
MARIA ROMERO <mariaromero18e@gmail.com>
2015-12-05 20:57:27 -0800

    updated styling to new article entry form page

--- new.js
+++ new.js
@@ -1,23 +1,23 @@
 var newEntry = {};
 
-$('#new-form').change(function() {
-  newEntry.articleTitle = $('#article-title').val();
-  newEntry.category = $('#article-category').val();
-  newEntry.author = $('#article-author').val();
-  newEntry.authorUrl = $('#article-author-url').val();
+$('#entryForm').change(function() {
+  newEntry.articleTitle = $('#articleTitle').val();
+  newEntry.category = $('#category').val();
+  newEntry.author = $('#author').val();
+  newEntry.authorUrl = $('#authorUrl').val();
   newEntry.publishedOn = new Date();
-  newEntry.articleBody = marked($('#article-body').val());
+  newEntry.articleBody = marked($('#articleBody').val());
 
   var previewTemplateScript = $('#article-template').html();
   var previewTemplate = Handlebars.compile(previewTemplateScript);
   var previewHTML = previewTemplate(newEntry);
 
-  $('#articles').empty().append(previewHTML);
+  $('#preview').empty().append(previewHTML);
   $('pre code').each(function (i, block){
     hljs.highlightBlock(block);
   });
 
   var newArticle = JSON.stringify(newEntry);
-  $('#article-json').val(newArticle);
+  $('#jsonString').val(newArticle);
 
 });
