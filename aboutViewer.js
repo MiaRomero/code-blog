9532ade48a3a43ad65c6cad6d953311741ce4599
MARIA ROMERO <mariaromero18e@gmail.com>
2015-12-16 17:21:37 -0800

    updated aboutViewer so branches reload on about page

--- aboutViewer.js
+++ aboutViewer.js
@@ -8,36 +8,43 @@ repos.RequestAll = function (callback){
     type:'GET',
     url: '/github/users/MiaRomero/repos',
   }).done(function(data){
-    console.log(data);
     repos.all = data;
   }).done(callback);
 };
 
-repos.branches = function (callback){
+repos.getBranches = function (callback){
   $.ajax({
     type:'GET',
     url: '/github/repos/MiaRomero/code-blog/branches',
-  
+
   }).done(function(data){
     repos.branches = data;
-    repos.branches.forEach(function (currentElement, index, array){
-      $('#branches').append($('<li>').text(array[index].name));
-    });
   }).done(callback);
 };
 
 var about = {};
 about.blurb = 'About me me me.  I\'m in Code 301.  This is my project.';
 
+repos.showRepos = function () {
+  repos.all.forEach(function (currentElement, index, array){
+    $('#repos').append($('<li>').text(array[index].full_name));
+  });
+};
+
+repos.showBranches = function () {
+  repos.branches.forEach(function (currentElement, index, array){
+    $('#branches').append($('<li>').text(array[index].name));
+  });
+};
+
 about.index = function () {
   $('.articles').hide();
   $('.about').empty().show();
   $('#repos').empty().show();
+  $('#branches').empty().show();
   $('.about').prepend('<p>' + about.blurb + '</p>');
   $('.about p').append('<p>Here are my GitHub repos: </p>');
-  repos.all.forEach(function (currentElement, index, array){
-    $('#repos').append($('<li>').text(array[index].full_name));
-  });
   $('#branches').text('And here are the branches on my current project repo: ');
-  repos.branches();
+  repos.getBranches(repos.showBranches);
+  repos.showRepos();
 };
